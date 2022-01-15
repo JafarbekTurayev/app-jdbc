371c9e30c01ed7ac2c3f5b53f86a60527c7b3c80
To'rayev Ja'farbek Uchqun o'gli <tjafarbek@mail.ru>
2022-01-15 09:30:31 +0500

    project started

---
+++ src/main/java/entity/Response.java
@@ -0,0 +1,14 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response {
+    private boolean success;
+    private String message;
+}
